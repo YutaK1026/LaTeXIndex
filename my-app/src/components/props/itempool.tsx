d0158b6ae801825be9b8d1abba9745c15683e452
kawachan <inheritans1026@gmail.com>
2024-02-16 17:34:09 +0900

    create-command一覧表示画面作成

--- my-app/src/components/props/itempool.tsx
+++ my-app/src/components/props/itempool.tsx
@@ -1,18 +1,43 @@
+"use client"
 import CommandCard from "@/components/props/commandcard"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import { useState, useEffect } from "react";
+import getImageListFromNotion from "@/components/props/image-from-notion";
+import SearchCommand from "@/components/props/search/search-command";
+
+interface NotionImageProps{
+  title: string
+  id: string
+  image: string
+  command: string
+  description: string
+}
 
 export default function ItemPool(){
-  const titleName = [["Title",`title{title}`,"タイトルを表示"], ["Author","\title{title}","著者を表示"], ["Date","\title{title}","日付を表す"], ["Title",`title{title}`,"タイトルを表示"], ["Title",`title{title}`,"タイトルを表示"], ["Title",`title{title}`,"タイトルを表示"], ["Title",`title{title}`,"タイトルを表示"], ["Author","\title{title}","著者を表示"], ["Title",`title{title}`,"タイトルを表示"], ["Author","\title{title}","著者を表示"], ]
+
+  const [data, setData] = useState<NotionImageProps[]>();
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = SearchCommand([""])
+      setData(result);
+      console.log(result)
+    };
+    fetchData();
+  }, []);
+
+  if(!data){return <div>Loading</div>}
+
   return(
     <ScrollArea className="h-full w-full ml-4">
     <div className="grid grid-flow-row-dense grid-cols-4 m-4">
-      {titleName.map((item, key) => {
+      {data.map((item, key) => {
         return(
-          <div className="m-3">
+          <div className="m-3" key={"key"+key}>
             <CommandCard 
-              title={item[0]}
-              command={item[1]}
-              description={item[2]}
+              title={item.title}
+              command={item.command}
+              description={item.description}
+              url={item.image}
             />
           </div>
         )
