0060187177157fe7a10c469c09e2742374c4bd77
kawachan <inheritans1026@gmail.com>
2024-02-16 17:33:37 +0900

    add-commandプレビューにプレビュー画像を貼るように成型

--- my-app/src/components/props/commandcard.tsx
+++ my-app/src/components/props/commandcard.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
     Card,
     CardContent,
@@ -18,14 +19,17 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
+import Image from 'next/image'
 
 interface CommandCardProps{
   title: string
   command: string
   description: string
+  url: string
 }
 
-export default function CommandCard({title, command, description}: CommandCardProps){
+export default function CommandCard({title, command, description, url}: CommandCardProps){
+  
   return(
     <Card>
       <CardHeader>
@@ -46,13 +50,25 @@ export default function CommandCard({title, command, description}: CommandCardPr
             </DialogDescription>
           </DialogHeader>
           <div className="flex items-center gap-4">
-            <div className="align-middle">command: </div>
+            <div className="align-middle">
+              command: 
+            </div>
             <div className="rounded-md border px-4 py-3 font-mono text-sm">
               \{command}
             </div>
           </div>
-          <div>
-            preview: 
+          <div className="flex items-center gap-4">
+            <div className="align-middle">
+              preview: 
+            </div>
+            <div className="rounded-md border px-4 py-3 font-serif text-sm"> 	
+              <Image
+                src={url}
+                width={100}
+                height={100}
+                alt={title}
+              />
+            </div>
           </div>
         </DialogContent>
       </Dialog>
