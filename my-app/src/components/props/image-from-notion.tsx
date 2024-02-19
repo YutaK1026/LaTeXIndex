3d255d4b5221cd74a7933ddffc4488dd8c6ae510
kawachan <inheritans1026@gmail.com>
2024-02-19 11:13:09 +0900

    chage-DB取得のみ記述

--- my-app/src/components/props/image-from-notion.tsx
+++ my-app/src/components/props/image-from-notion.tsx
@@ -1,5 +1,3 @@
 export default async function getImageListFromNotion(){
-  const res = await fetch('/api/notion');
-  const items = await res.json();
-  return items
+  await fetch('/api/notion');
 }
