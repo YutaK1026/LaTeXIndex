cbbf68123d131184466f6d3661609f7c73db1496
kawachan <inheritans1026@gmail.com>
2024-02-19 11:14:54 +0900

    add-タグごとにサイドメニューに表示されるように修正

--- my-app/src/components/props/sidemenu.tsx
+++ my-app/src/components/props/sidemenu.tsx
@@ -1,47 +1,48 @@
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
 import { Button } from "@/components/ui/button"
+import SplitByTag from "@/components/props/search/split-by-tag"
 
-const tags = ["A", "B", "C", "D"]
-export default function SideMenu(){
-	return(
-		<ScrollArea className="h-full w-full ml-4">
-			<div className="text-xl">
-				Document Classes
-			</div>
-			<Separator/>
-			{tags.map((tag) => (
-				<div key={tag} className="text-sm p-3 bg-color-gray">
-					{tag}
-				</div>
-			))}
-			<div className="text-xl">
-				Common DocumentClass Options
-			</div>
-			<Separator />
-			{tags.map((tag) => (
-				<div key={tag} className="text-sm p-3">
-					{tag}
-				</div>
-			))}
-			<div className="text-xl">
-				Packages
-			</div>
-			<Separator />
-			{tags.map((tag) => (
-				<div key={tag} className="text-sm p-3">
-					{tag}
-				</div>
-			))}
-			<div className="text-xl">
-				Title
-			</div>
-			<Separator />
-			{tags.map((tag) => (
-				<div key={tag} className="text-sm p-3">
-					{tag}
-				</div>
-			))}
-		</ScrollArea>
-	)
+interface NotionImageProps {
+  title: string
+  id: string
+  image: string
+  command: string
+  description: string
+  tag: string
+}
+type CommandListProps = Record<string, NotionImageProps[]>
+
+export default function SideMenu({setWord}: {setWord: (word: string)=>void}) {
+  const notionData = SplitByTag([""])
+  
+  const selectTag = (title: string) => {
+    setWord(title)
+  }
+
+  return (
+    <ScrollArea className="h-full w-full ml-4">
+      {notionData.map((item: CommandListProps, key: string) => {
+        const keyName = Object.keys(item)[0]
+        var commandList: NotionImageProps[] = []
+        item[keyName].map((i) => { commandList.push(i) })
+        return (
+          <div key={key}>
+            <div className="text-xl">
+              {keyName}
+            </div>
+            <Separator />
+            {commandList.map((i: NotionImageProps, key) => {
+              return (
+                <div key={key} className="text-sm p-3 bg-color-gray" onClick={() => {selectTag(i.title)}}>
+                  {i.title}
+                </div>
+              )
+            })}
+          </div>
+        )
+
+      })}
+    </ScrollArea>
+  )
 }
