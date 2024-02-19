306a9dbb9e7b18514f0db94e0bf2403490dcba5a
kawachan <inheritans1026@gmail.com>
2024-02-19 11:12:16 +0900

    add-HeaderにDB更新ボタンを追加-本番環境では削除

--- my-app/src/components/pages/header.tsx
+++ my-app/src/components/pages/header.tsx
@@ -1,7 +1,16 @@
-export default function Header(){
-  return(
-    <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
-      LaTeX with React
-    </h2>
+"use client";
+import { Button } from "@/components/ui/button"
+import getImageListFromNotion from "@/components/props/image-from-notion"
+
+export default function Header() {
+  return (
+    <div className="flex">
+      <h2 className="flex-auto scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
+        LaTeX with React
+      </h2>
+      <Button className="m-3" onClick={getImageListFromNotion}>
+        データ更新取得
+      </Button>
+    </div>
   )
 }
