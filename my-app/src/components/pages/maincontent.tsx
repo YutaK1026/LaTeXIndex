791f87680e5bd7f1c16ca1f941d44ae189a69f7a
kawachan <inheritans1026@gmail.com>
2024-02-19 11:12:42 +0900

    add-検索欄及び左タブで要素を選択した場合の結果を反映するよう修正

--- my-app/src/components/pages/maincontent.tsx
+++ my-app/src/components/pages/maincontent.tsx
@@ -1,37 +1,47 @@
+"use client"
 import {
-    ResizableHandle,
-    ResizablePanel,
-    ResizablePanelGroup,
-  } from "@/components/ui/resizable"
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 import SearchLocation from "@/components/props/searchlocaion";
 import SideMenu from "@/components/props/sidemenu";
 import ItemPool from "@/components/props/itempool";
 import Header from "@/components/pages/header";
+import { useState } from "react";
 
-export default function MainContent(){
-	return(
-		<ResizablePanelGroup 
-		direction="horizontal">
-			<ResizablePanel defaultSize={25}>
-				<div className="flex h-screen items-center justify-center p-1">
-					<SideMenu />
-				</div>
-			</ResizablePanel>
-			<ResizablePanel defaultSize={75}>
-				<ResizablePanelGroup
-					direction="vertical">
-					<ResizablePanel defaultSize={15}>
-						<div className="flex h-full ml-10">
-							<SearchLocation />
-						</div>
-					</ResizablePanel>
-					<ResizablePanel defaultSize={85}>
-						<div className="flex h-full p-6">
-							<ItemPool />
-						</div>
-					</ResizablePanel>
-				</ResizablePanelGroup>
-			</ResizablePanel>
-		</ResizablePanelGroup>	
-	)
+export default function MainContent() {
+  const [searchWordList, setSearchWordList] = useState<string[]>([""])
+
+  const setWord = (word: string) => {
+    const wordList = word.split(' ')
+    setSearchWordList(wordList)
+    //COMMENT: 検索欄に入力したワードをitempoolに送信する
+  }
+
+  return (
+    <ResizablePanelGroup
+      direction="horizontal">
+      <ResizablePanel defaultSize={25}>
+        <div className="flex h-screen items-center justify-center p-1">
+          <SideMenu setWord={setWord}/>
+        </div>
+      </ResizablePanel>
+      <ResizablePanel defaultSize={75}>
+        <ResizablePanelGroup
+          direction="vertical">
+          <ResizablePanel defaultSize={15}>
+            <div className="flex h-full ml-10">
+              <SearchLocation setWord={setWord}/>
+            </div>
+          </ResizablePanel>
+          <ResizablePanel defaultSize={85}>
+            <div className="flex h-full p-6">
+              <ItemPool searchWordList = {searchWordList}/>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  )
 }
