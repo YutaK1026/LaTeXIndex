6da2c7107c1efaa8ae9a81c3a6d872c75069d12f
kawachan <inheritans1026@gmail.com>
2024-02-19 11:13:52 +0900

    add-検索欄に入力した文字を結果に反映するように修正

--- my-app/src/components/props/searchlocaion.tsx
+++ my-app/src/components/props/searchlocaion.tsx
@@ -1,21 +1,25 @@
 'use client';
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
 
-export default function SearchLocation(){
-  const  handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.nativeEvent.isComposing || e.key !== 'Enter') return
-    console.log("Enterキーが押された")
+export default function SearchLocation({setWord}: {setWord: (word: string)=>void}) {
+  const handleKeyDown = (e: React.KeyboardEvent<EventTarget> & React.ChangeEvent<HTMLInputElement>) => {
+    setWord(e.target.value)
+  }
+  const onFocusFunc = (e: string) => {
+    setWord(e)
   }
 
-  return(
+  return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
       <Label htmlFor="name">検索BOX</Label>
       <Input
         type="text"
-        onKeyDown={handleKeyDown}
-        placeholder="検索ワードを入力してください" 
+        id="input"
+        value=""
+        onChange={handleKeyDown}
+        placeholder="検索ワードを入力してください"
+        onFocus={(e) => {onFocusFunc(e.target.value)}}
       />
     </div>
   )
