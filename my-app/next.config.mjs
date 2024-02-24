f63b52cb90eb302b1c0fea09ed75a07d44f570ef
kawachan <inheritans1026@gmail.com>
2024-02-24 10:31:34 +0900

    add-config及びpackage.json追加

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -3,6 +3,24 @@ const nextConfig = {
   images: {
     domains: ["pub-ee7d57261fe04297bd2ce9e0abe5eb8c.r2.dev"],
   },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.module.rules.unshift({
+      test: /pdf\.worker\.(min\.)?js/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[contenthash].[ext]',
+            publicPath: '/_next/static/worker',
+            outputPath: 'static/worker',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
