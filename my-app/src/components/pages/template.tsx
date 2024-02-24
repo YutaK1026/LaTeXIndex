74f6d9a7f975040306845673ee8b146483053ccb
kawachan <inheritans1026@gmail.com>
2024-02-24 10:59:44 +0900

    add-templateページのひな型作成

---
+++ my-app/src/components/pages/template.tsx
@@ -0,0 +1,60 @@
+"use client"
+import Header from "@/components/pages/header";
+import { Document, Page, pdfjs } from 'react-pdf';
+import { useResizeObserver } from '@wojtekmaj/react-hooks';
+import { useState, useCallback } from "react";
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const options = {
+  cMapUrl: '/cmaps/',
+  standardFontDataUrl: '/standard_fonts/',
+};
+const resizeObserverOptions = {};
+
+const maxWidth = 800;
+
+type PDFFile = string | File | null;
+
+export default function TemplatePage() {
+  const [file, setFile] = useState<PDFFile>('./test.pdf');
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+  
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setNumPages(numPages);
+  };
+  return (
+    <div>
+      <Header />
+      {/* <div className="Example__container"> */}
+        <div className="Example__container__document" ref={setContainerRef}>
+          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+              />
+            ))}
+          </Document>
+        </div>
+      {/* </div> */}
+    </div>
+  )
+}
