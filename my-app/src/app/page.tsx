bd4d528530a443d5a2df17a54e19faedde40e153
kawachan <inheritans1026@gmail.com>
2024-03-29 17:07:44 +0900

    deployテスト3

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -52,8 +52,7 @@ export default function Home() {
             }
           </ResizablePanel>
           <ResizablePanel defaultSize={90}>
-            aaaaa
-            {/* <MainContent searchWordListFromHeader={searchWordList}/> */}
+            <MainContent searchWordListFromHeader={searchWordList}/>
           </ResizablePanel>
         </ResizablePanelGroup>
       </div>
